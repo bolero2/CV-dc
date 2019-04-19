a8d7ccfc3b78b5329914ebd0b92cad5bf0cd07a6
sheocjf1025 <41134624+sheocjf1025@users.noreply.github.com>
2019-04-19 17:19:38 +0900

    Create hand_masking-opencv-python.py

---
+++ hand_masking-opencv-python.py
@@ -0,0 +1,24 @@
+import cv2 as cv
+
+# img = cv.imread("C:\\Users\\Administrator\\Desktop\\testImage2.jpg")
+cap = cv.VideoCapture(0)
+cv.namedWindow("video")
+
+
+def cvtimg(img):
+    ycrcb = cv.cvtColor(img, cv.COLOR_BGR2YCrCb)
+    range = cv.inRange(src=ycrcb, lowerb=(0, 133, 77), upperb=(255, 173, 127))
+    blur = cv.GaussianBlur(src=range, ksize=(31, 31), sigmaX= 4)
+    _, th1 = cv.threshold(src=blur, thresh=127, maxval=255, type=cv.THRESH_BINARY)
+
+    return th1
+
+
+ret, frame = cap.read()
+
+while ret:
+    ret, frame = cap.read()
+    cv.imshow("video", cvtimg(frame))
+    cv.waitKey(10)
+
+exit(0)
