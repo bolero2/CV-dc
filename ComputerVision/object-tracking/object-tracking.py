ab8ac3c9fe91c61c6527ee85fb7f310506cb66ad
bolero2 <sheocjf1025@gmail.com>
2021-01-20 14:44:32 +0900

    Modified object-tracking.py

--- ComputerVision/object-tracking/object-tracking.py
+++ ComputerVision/object-tracking/object-tracking.py
@@ -2,14 +2,16 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
-tracker = cv2.TrackerMOSSE_create()
+tracker = cv2.TrackerCSRT_create()
 ret, img = cap.read()
-bbox = cv2.selectROI("test", img, False)
+bbox = cv2.selectROI("test", img)
+print(f'selected ROI= {bbox}')
 tracker.init(img, bbox)
 
 
-def drawBox():
-    pass
+def drawBox(img, bbox):
+    print(bbox)
+    return cv2.rectangle(img, pt1=(int(bbox[0]), int(bbox[1])), pt2=(int(bbox[0]) + int(bbox[2]), int(bbox[1]) + int(bbox[3])), color=(0, 0, 255), thickness=3)
 
 
 while ret:
@@ -17,11 +19,12 @@ while ret:
     ret, img = cap.read()
 
     ret, bbox = tracker.update(img)
+    
     if ret:
-        drawBox()
+        img = drawBox(img, bbox)
     else:
         cv2.putText(img, "Lost", (75, 75), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
-
+    
     fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
     cv2.putText(img, f"FPS : {str(int(fps))}", (75, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
     cv2.imshow("test", img)
